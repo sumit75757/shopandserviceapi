e725e52e1d0570147c7cc9756fc8c757bbd0746d
sumit75757 <patelsumit75757@gmail.com>
2022-12-08 22:28:40 +0530

    asdfas

--- src/auth/otp/otpEmail.controller.ts
+++ src/auth/otp/otpEmail.controller.ts
@@ -1,46 +1,81 @@
 import main from "../sender";
 import otpGenerator from "otp-generator";
-let otp: any = '';
+let otp: any = "";
 require("dotenv").config();
 
+const otpdata: any[] = [];
+
 const emailservice = {
-    email(req: any, res: any) {
-       
-        otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, digits: true, lowerCaseAlphabets: false })
-        console.log("otp is", otp);
-        console.log(req.body);
-        const html: string = `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2"><div style="margin:50px auto;width:70%;padding:20px 0"><div style="border-bottom:1px solid #eee"><a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a></div><p style="font-size:1.1em">Hi,</p><p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">`+otp+`</h2><p style="font-size:0.9em;">Regards,<br />Your Brand</p><hr style="border:none;border-top:1px solid #eee" /><div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300"><p>Your Brand Inc</p><p>1600 Amphitheatre Parkway</p><p>California</p></div></div></div>`
+  email(req: any, res: any) {
+    otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      specialChars: false,
+      digits: true,
 
-        const email = ({
-            from: process.env.GMAIL,
-            to: req.body.email,
-            subject: +"otp is" + otp,
-            text: otp,
-            // amp: '',
-            html: html,
-        });
-        main(email).then(emailsend => {
-             res.status(200).json(emailsend);
-        }).catch(Err => {
-             res.status(400).json(Err);
-        });
-    },
+      lowerCaseAlphabets: false,
+    });
+    console.log("otp is", otp);
+    console.log(otpdata);
+    const html: string =
+      `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2"><div style="margin:50px auto;width:70%;padding:20px 0"><div style="border-bottom:1px solid #eee"><a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a></div><p style="font-size:1.1em">Hi,</p><p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">` +
+      otp +
+      `</h2><p style="font-size:0.9em;">Regards,<br />Your Brand</p><hr style="border:none;border-top:1px solid #eee" /><div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300"><p>Your Brand Inc</p><p>1600 Amphitheatre Parkway</p><p>California</p></div></div></div>`;
 
-    verify(req: any, res: any) {
-        if (req.body.otp == otp) {
-            res.status(200).json({
-                status: true,
-                messge: 'You has been successfully registered',
-            })
-        }
-        else {
-            res.status(400).json({
-                status: false,
-                messge: 'You has not been registered',
-            })
-        }
+    const email = {
+      from: process.env.GMAIL,
+      to: req.body.email,
+      subject: "otp is " + otp,
+      text: otp,
+      // amp: '',
+      html: html,
+    };
+    main(email)
+      .then((emailsend) => {
+        otpdata.push({
+          email: req.body.email,
+          otp: otp,
+          time: new Date().getTime(),
+        });
+        res.status(200).json(emailsend);
+      })
+      .catch((Err) => {
+        res.status(400).json(Err);
+      });
+  },
+
+  verify(req: any, res: any) {
+    console.log(otpdata);
+    console.log(new Date().getTime());
+
+    let getotps: any = otpdata.filter((d) => (req.body.email = d.email));
+    console.log(getotps);
+
+    if (getotps) {
+      getotps[0].otp == req.body.otp
+        ? res.status(200).json({
+            status: true,
+            messge: "You has been successfully registered",
+          })
+        : res.status(400).json({
+            status: false,
+            messge: "You has not been registered",
+          });
     }
+  },
+};
+
+export default emailservice;
+
+function deleteOtp() {
+  otpdata.forEach((element) => {
+    console.log("asdgasdf", element);
+    if (new Date().getTime() > element.time) {
+      let deletedata = otpdata.filter((item) => item.email !== element.email);
+      console.log("asdgaasdfasdf", deletedata);
+    } else {
+      console.log("not done");
+    }
+    console.log(new Date().getTime());
+  });
 }
-
-
-export default emailservice
+const get = setInterval(deleteOtp, 300000);
---
+++ src/auth/token.ts
@@ -0,0 +1,24 @@
+import { google } from "googleapis";
+import path from "path";
+import fs from "fs";
+import credentials from "./cradansial.json";
+require("dotenv").config();
+
+const redirect_uri = window.location.origin
+// Replace with the code you received from Google
+let code =
+  "4/0AfgeXvuL2FQdGNW0m8hZ-HxW6Z-SGAilZpb-xq1hQU8TS2VwjUKAfyCj_pv4Zaj58hgAQg";
+const { client_secret, client_id } = credentials.web;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uri+"/"
+);
+ oAuth2Client.getToken(code).then(({ tokens }: any) => {
+  const tokenPath = path.join(__dirname, "token.json");
+  fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+  console.log("Access token and refresh token stored to token.json");
+});
+
+
+
